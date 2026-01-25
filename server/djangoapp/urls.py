539d96ffabf803c2dc27a7ec7771dc51b1cd4980
samo-r <mukisasamuelr1@gmail.com>
2026-01-25 14:10:54 -0500

    Added logout functionality and Register functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
 
     # path for login
     path(route='login/', view=views.login_user, name='login'),
-
+    path(route='logout/', view=views.logout_request, name='logout'),
+    path(route='register/', view=views.registration, name='register'),
     # path for dealer reviews view
 
     # path for add a review view
