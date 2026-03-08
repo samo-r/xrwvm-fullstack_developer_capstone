983ef81e56b9b6e2f74021ed44645498361e9a4c
samo-r <mukisasamuelr1@gmail.com>
2026-03-08 17:40:55 -0400

    Fixed the csrf issue on the add_review view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -121,6 +121,7 @@ def get_dealer_reviews(request, dealer_id):
         return JsonResponse({"status":400,"message":"Bad Request"})
         
 # Add new review view
+@csrf_exempt
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
